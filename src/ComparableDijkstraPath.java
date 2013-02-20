4a6e1e4365a63bfb74bab1ba6fb1e08acc183c90
Anno <anno.tao@gmail.com>
2013-02-20 16:45:27 +0100

    Last version plus class?

--- src/ComparableDijkstraPath.java
+++ src/ComparableDijkstraPath.java
@@ -1,28 +1,22 @@
-import java.security.InvalidParameterException;
+import java.util.ArrayList;
 
 
-public class ComparableDijkstraPath extends BusEdge implements Comparable{
-
-	ComparableDijkstraPath(int from, int to, double weight, String line) {
-		super(from, to, weight, line);
+public class ComparableDijkstraPath implements Comparable{
+	private ArrayList<BusStop> path;
+	private int totalWeight;
+	
+	ComparableDijkstraPath(ArrayList<BusStop> stops) {
+		this.path = stops;
+		int count = 0;
+		for (BusStop b : path) {
+			count++;
+		}
+		
 	}
 
 	@Override
 	public int compareTo(Object obj) {
-		if (obj != null) {
-			throw new NullPointerException("Pointer is null");
-		}
-		
-		if (!(obj instanceof ComparableDijkstraPath)) {
-			throw new InvalidParameterException("Not a valid parameter");
-		}
-		ComparableDijkstraPath object = (ComparableDijkstraPath) obj;
-		double cmp = (this.weight - object.weight);
-		if (cmp == 0) {
-			return 0;
-		} else {
-			return cmp > 0 ? (int)(cmp+1) : (int)(cmp-1);
-		}
+		return 0;
 	}
 	
 }
--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -2,14 +2,28 @@
 import java.util.*;
 
 public class DirectedGraph<E extends Edge> {
+	private int size;
+	private List<E>[] neighbours;
 
-
+	@SuppressWarnings("unchecked")
 	public DirectedGraph(int noOfNodes) {
-		;
+		neighbours = (List<E>[]) new LinkedList[noOfNodes];
 	}
-
+	
 	public void addEdge(E e) {
-		;
+		List<E> insert;
+		if (neighbours[e.from] == null) {
+			insert = new LinkedList<E>();
+			insert.add(e);
+			size++;
+			neighbours[e.from] = insert;
+		} else {
+			List<E> list = neighbours[e.from];
+			if (!list.contains(e)) {
+				list.add(e);
+				size++;
+			}
+		}
 	}
 
 	public Iterator<E> shortestPath(int from, int to) {
