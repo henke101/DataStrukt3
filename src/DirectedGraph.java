fdbfc241f5aa54b187de3b99bf5af200f6941f23
Anno <anno.tao@gmail.com>
2013-02-25 15:05:06 +0100

    Added the beginnings of ComparableDijkstraPath and fixed a few things in DirectedGraph

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -2,8 +2,33 @@
 import java.util.*;
 
 public class DirectedGraph<E extends Edge> {
+	protected class ComparableDijkstraPath implements Comparable {
+		private int node;
+		private int cost;
+		private List<E> path;
+		
+		public ComparableDijkstraPath(int node, int cost, List<E> path) {
+			this.node = node;
+			this.cost = cost;
+			this.path = path;
+		}
+		
+		@SuppressWarnings("unchecked")
+		@Override
+		public int compareTo(Object obj) throws IllegalArgumentException {
+			if (obj == null || !(obj instanceof DirectedGraph.ComparableDijkstraPath)) {
+				throw new IllegalArgumentException();
+			}
+			ComparableDijkstraPath object = null;
+			object = (ComparableDijkstraPath) obj;
+			
+			return this.cost - object.cost;
+		}
+		
+	}
+	
 	private int size;
-	private List<E>[] neighbours;
+	protected List<E>[] neighbours;
 
 	@SuppressWarnings("unchecked")
 	public DirectedGraph(int noOfNodes) {
@@ -27,6 +52,21 @@ public class DirectedGraph<E extends Edge> {
 	}
 
 	public Iterator<E> shortestPath(int from, int to) {
+/*lägg (startnod, 0, tom väg) i en p-kö
+while kön inte är tom
+(nod, cost, path) = första elementet i p-kön
+if nod ej är besökt
+if nod är slutpunkt returnera väg
+else 
+markera nod besökt
+for every v on EL(nod)
+if v ej är besökt
+lägg in nytt köelement 
+för v i p-kön
+ * 
+ */
+		PriorityQueue<ComparableDijkstraPath> pq = new PriorityQueue<ComparableDijkstraPath>();
+		
 		return null;
 	}
 		
